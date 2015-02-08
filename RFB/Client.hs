a4df4ea67e6170b8513a002bc708374ea98bdb11
Natalie Perna <nmperna@gmail.com>
2015-02-08 17:25:20 -0500

    Read pixel data. Organize stuff into records. Abstract hi/loByte function for all Int sizes.

--- RFB/Client.hs
+++ RFB/Client.hs
@@ -6,6 +6,46 @@ import qualified Data.ByteString.Char8 as B8
 import Data.Char (ord, chr)
 import Data.Bits
 
+data RFBFormat = RFBFormat { encodingTypes :: [Int]
+                           , bitsPerPixel :: Int
+                           , depth :: Int
+                           , bigEndianFlag :: Int
+                           , trueColourFlag :: Int
+                           , redMax :: Int
+                           , greenMax :: Int
+                           , blueMax :: Int
+                           , redShift :: Int
+                           , greenShift :: Int
+                           , blueShift :: Int
+                           } deriving (Show)
+
+data Box = Box { x :: Int
+               , y :: Int
+               , w :: Int
+               , h :: Int
+               } deriving (Show)
+
+data Pixel = Pixel { r :: Int
+                   , g :: Int
+                   , b :: Int
+                   } deriving (Show)
+
+data Rectangle = Rectangle { rectangle :: Box
+                           , pixels :: [Pixel]
+                           }
+
+format = RFBFormat { encodingTypes = [0]
+                   , bitsPerPixel = 32
+                   , depth = 24
+                   , bigEndianFlag = 0
+                   , trueColourFlag = 1
+                   , redMax = 255
+                   , greenMax = 255
+                   , blueMax = 255
+                   , redShift = 0
+                   , greenShift = 8
+                   , blueShift =  16 }
+
 connect :: String -> Int -> IO()
 connect host port = withSocketsDo $ do
 
@@ -34,7 +74,6 @@ connect host port = withSocketsDo $ do
     putStrLn $ "Server Security Types: " ++ show securityTypes
 
     -- TODO Actually check security types before blindy choosing
-    --send sock (intsToBytestring [1])
     sendInts sock [1]
 
     -- I don't know why SecurityResult isn't being sent
@@ -44,58 +83,87 @@ connect host port = withSocketsDo $ do
     sendInts sock [1]
 
     -- Get ServerInit message
-    (fbW1:fbW2:
-        fbH1:fbH2:
-        bitsPerPixel:
-        depth:
-        bigEndianFlag:
-        trueColourFlag:
-        redMax1:redMax2:
-        greenMax1:greenMax2:
-        blueMax1:blueMax2:
-        redShift:
-        greenShift:
-        blueShift:
-        _:_:_: -- padding
-        len1:len2:len3:len4:
-        _) <- recvInts sock 24
-    let framebufferWidth = bytesToInt [fbW1, fbW2]
-    let framebufferHeight = bytesToInt [fbH1, fbH2]
-    let redMax = bytesToInt [redMax1, redMax2]
-    let greenMax = bytesToInt [greenMax1, greenMax2]
-    let blueMax = bytesToInt [blueMax1, blueMax2]
-    let nameLength = bytesToInt [len1, len2, len3, len4]
+    (w1:w2:
+     h1:h2:
+     _:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_: -- server-pixel-format
+     l1:l2:l3:l4:
+     _) <- recvInts sock 24
 
-    serverName <- recvString sock nameLength
+    let framebuffer = Box { x = 0
+                          , y = 0
+                          , w = bytesToInt [w1, w2]
+                          , h = bytesToInt [h1, h2] }
 
-    putStrLn $ "framebufferWidth: " ++ show framebufferWidth
-    putStrLn $ "framebufferHeight: " ++ show framebufferHeight
-    putStrLn $ "bitsPerPixel: " ++ show bitsPerPixel
-    putStrLn $ "depth: " ++ show depth
-    putStrLn $ "bigEndianFlag: " ++ show bigEndianFlag
-    putStrLn $ "trueColourFlag: " ++ show trueColourFlag
-    putStrLn $ "redMax: " ++ show redMax
-    putStrLn $ "greenMax: " ++ show greenMax
-    putStrLn $ "blueMax: " ++ show blueMax
-    putStrLn $ "redShift: " ++ show redShift
-    putStrLn $ "greenShift: " ++ show greenShift
-    putStrLn $ "blueShift: " ++ show blueShift
-    putStrLn $ "nameLength: " ++ show nameLength
-    putStrLn $ "serverName: " ++ serverName
+    -- Get ServerName
+    serverName <- recvString sock (bytesToInt [l1, l2, l3, l4])
 
-    let framebufferUpdateRequest =
-            [ 3, 0, 0, 0, 0, 0
-            , hiByte framebufferWidth, loByte framebufferWidth
-            , hiByte framebufferHeight, loByte framebufferHeight ]
-    sendInts sock framebufferUpdateRequest
-    (messageType:_:nR1:nR2:_) <- recvInts sock 4
-    let numberofRectangles = bytesToInt [nR1, nR2]
-    putStrLn $ "numberofRectangles: " ++ show numberofRectangles
+    putStrLn $ "Server Name: " ++ serverName
+    putStrLn $ "Framebuffer: " ++ show framebuffer
+    putStrLn $ "Encoding and pixel format: " ++ show format
+
+    setEncodings sock format
+    setPixelFormat sock format
+
+    framebufferUpdateRequest sock 0 framebuffer
+
+    (_:_:n1:n2:_) <- recvInts sock 4
+    let numberofRectangles = bytesToInt [n1, n2]
+
+    -- First rectangle
+    (x1:x2:
+     y1:y2:
+     w1:w2:
+     h1:h2:
+     _) <- recvInts sock 12
+    let box1 = Box { x = bytesToInt[x1, x2]
+                   , y = bytesToInt[y1, y2]
+                   , w = bytesToInt [w1, w2]
+                   , h = bytesToInt [h1, h2] }
+
+    colours <- fmap rawToPixels (recvInts sock (w box1 * h box1 * ((bitsPerPixel format) `div` 8)))
+
+    let rectangle1 = Rectangle { rectangle = box1
+                                , pixels = colours }
+
+    putStrLn $ "First Pixel: " ++ show (head (pixels rectangle1))
 
     hold <- getLine
 
     -- Close socket
     sClose sock
+
+setEncodings :: Socket -> RFBFormat -> IO Int
+setEncodings sock format =
+    sendInts sock ([ 2     -- message-type
+                   , 0 ]    -- padding
+                   ++ intToBytes 2 (length (encodingTypes format))
+                   ++ concat (map (intToBytes 4) (encodingTypes format)))
+
+setPixelFormat :: Socket -> RFBFormat -> IO Int
+setPixelFormat sock format =
+    sendInts sock ([ 0         -- message-type
+                   , 0, 0, 0   -- padding
+                   , bitsPerPixel format
+                   , depth format
+                   , bigEndianFlag format
+                   , trueColourFlag format ]
+                   ++ intToBytes 2 (redMax format)
+                   ++ intToBytes 2 (greenMax format)
+                   ++ intToBytes 2 (blueMax format)
+                   ++
+                   [ redShift format
+                   , greenShift format
+                   , blueShift format
+                   , 0, 0, 0 ]) -- padding
+
+framebufferUpdateRequest :: Socket -> Int -> Box -> IO Int
+framebufferUpdateRequest sock incremental framebuffer =
+    sendInts sock ([ 3  -- message-type
+                   , incremental]
+                   ++ intToBytes 2 (x framebuffer)
+                   ++ intToBytes 2 (y framebuffer)
+                   ++ intToBytes 2 (w framebuffer)
+                   ++ intToBytes 2 (h framebuffer))
 
 bytestringToInts :: B8.ByteString -> [Int]
 bytestringToInts b = map ord (B8.unpack b)
@@ -119,8 +187,11 @@ bytesToInt :: [Int] -> Int
 bytesToInt [] = 0
 bytesToInt b = shiftL (bytesToInt (init b)) 8 .|. (last b)
 
-hiByte :: Int -> Int
-hiByte b = shiftR (b .&. 0xFF00) 8
+intToBytes :: Int -> Int -> [Int]
+intToBytes 0 _ = []
+intToBytes l 0 = 0 : intToBytes (l-1) 0
+intToBytes l b = intToBytes (l-1) (shiftR (b .&. 0xFF00) 8) ++ [ b .&. 0xFF ]
 
-loByte :: Int -> Int
-loByte b = b .&. 0xFF
+rawToPixels :: [Int] -> [Pixel]
+rawToPixels [] = []
+rawToPixels (r:g:b:a:t) = (Pixel r g b) : rawToPixels t
