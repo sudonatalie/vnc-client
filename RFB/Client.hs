dcc5802e570724ca12d425828121bc8356c35191
Natalie Perna <nmperna@gmail.com>
2015-02-08 22:32:46 -0500

    Almost displaying screen in an X window, but it's slow and is missing the bottom of the screen.

--- RFB/Client.hs
+++ RFB/Client.hs
@@ -6,6 +6,10 @@ import qualified Data.ByteString.Char8 as B8
 import Data.Char (ord, chr)
 import Data.Bits
 
+import Graphics.X11.Xlib
+import System.Exit (exitWith, ExitCode(..))
+import Control.Concurrent (threadDelay)
+
 data RFBFormat = RFBFormat
     { encodingTypes :: [Int]
     , bitsPerPixel :: Int
@@ -27,16 +31,13 @@ data Box = Box
     , h :: Int
     } deriving (Show)
 
-data Pixel = Pixel
-    { r :: Int
+data PixelPoint = PixelPoint
+    { px :: Int
+    , py :: Int
+    , r :: Int
     , g :: Int
     , b :: Int
     } deriving (Show)
-
-data Rectangle = Rectangle
-    { rectangle :: Box
-    , pixels :: [Pixel]
-    }
 
 format = RFBFormat
     { encodingTypes = [0]
@@ -64,19 +65,15 @@ connect host port = withSocketsDo $ do
     sendInts sock []
     msg <- recvString sock 12
     -- TODO Verify version format
-    putStr $ "Server Protocol Version: " ++ msg
 
     -- TODO Actually compare version numbers before blindy choosing
-    let version = "RFB 003.007\n"
-    putStr $ "Requsted Protocol Version: " ++ version
-    sendString sock version
+    sendString sock "RFB 003.007\n"
 
     -- Receive number of security types
     (numberOfSecurityTypes:_) <- recvInts sock 1
 
     -- Receive security types
     securityTypes <- recvInts sock numberOfSecurityTypes
-    putStrLn $ "Server Security Types: " ++ show securityTypes
 
     -- TODO Actually check security types before blindy choosing
     sendInts sock [1]
@@ -102,40 +99,66 @@ connect host port = withSocketsDo $ do
     -- Get ServerName
     serverName <- recvString sock (bytesToInt [l1, l2, l3, l4])
 
-    putStrLn $ "Server Name: " ++ serverName
-    putStrLn $ "Framebuffer: " ++ show framebuffer
-    putStrLn $ "Encoding and pixel format: " ++ show format
-
     setEncodings sock format
     setPixelFormat sock format
 
     framebufferUpdateRequest sock 0 framebuffer
 
-    (_:_:n1:n2:_) <- recvInts sock 4
-    let numberofRectangles = bytesToInt [n1, n2]
+    display <- openDisplay ""
+    rootw <- rootWindow display (defaultScreen display)
+    win <- mkUnmanagedWindow display (defaultScreenOfDisplay display) rootw 0 0 (fromIntegral (w framebuffer)) (fromIntegral (h framebuffer))
+    setTextProperty display win "VNC Client" wM_NAME
+    mapWindow display win
 
-    -- First rectangle
+    (_:_:n1:n2:_) <- recvInts sock 4
+
+    putStrLn "Wait a little while for the buffer to fill, then press [Enter]..."
+    hold <- getLine
+
+    -- TODO Kind of almost working, but it only displays a slice of the top...
+    displayRectangles display win sock (bytesToInt [n1, n2])
+
+    sync display False
+    threadDelay (10 * 1000000)
+    exitWith ExitSuccess
+
+    -- Close socket
+    sClose sock
+
+displayRectangles :: Display -> Window -> Socket -> Int -> IO ()
+displayRectangles _ _ _ 0 = return ()
+displayRectangles display win sock n = do
     (x1:x2:
      y1:y2:
      w1:w2:
      h1:h2:
      _) <- recvInts sock 12
-    let box1 = Box { x = bytesToInt [x1, x2]
+    let rect = Box { x = bytesToInt [x1, x2]
                    , y = bytesToInt [y1, y2]
                    , w = bytesToInt [w1, w2]
                    , h = bytesToInt [h1, h2] }
+    pixels <- fmap (rawToPixelPoints rect) (recvInts sock (w rect * h rect * ((bitsPerPixel format) `div` 8)))
+    displayPixels display win pixels
+    displayRectangles display win sock (n-1)
 
-    colours <- fmap rawToPixels (recvInts sock (w box1 * h box1 * ((bitsPerPixel format) `div` 8)))
+displayPixels :: Display -> Window -> [PixelPoint] -> IO ()
+displayPixels display win [] = return ()
+displayPixels display win ((PixelPoint x y r g b):pixels) = do
+    gc <- createGC display win
+    setForeground display gc (fromIntegral (bytesToInt [r, g, b]))
+    drawPoint display win gc (fromIntegral x) (fromIntegral y)
+    freeGC display gc
+    displayPixels display win pixels
 
-    let rectangle1 = Rectangle { rectangle = box1
-                                , pixels = colours }
-
-    putStrLn $ "First Pixel: " ++ show (head (pixels rectangle1))
-
-    hold <- getLine
-
-    -- Close socket
-    sClose sock
+mkUnmanagedWindow :: Display -> Screen -> Window -> Position -> Position -> Dimension -> Dimension -> IO Window
+mkUnmanagedWindow d s rw x y w h = do
+  let visual = defaultVisualOfScreen s
+      attrmask = cWOverrideRedirect
+  allocaSetWindowAttributes $
+         \attributes -> do
+           set_override_redirect attributes True
+           createWindow d rw x y w h 0 (defaultDepthOfScreen s)
+                        inputOutput visual attrmask attributes
 
 setEncodings :: Socket -> RFBFormat -> IO Int
 setEncodings sock format =
@@ -197,6 +220,10 @@ intToBytes 0 _ = []
 intToBytes l 0 = 0 : intToBytes (l-1) 0
 intToBytes l b = intToBytes (l-1) (shiftR (b .&. 0xFF00) 8) ++ [ b .&. 0xFF ]
 
-rawToPixels :: [Int] -> [Pixel]
-rawToPixels [] = []
-rawToPixels (r:g:b:a:t) = (Pixel r g b) : rawToPixels t
+rawToPixelPoints :: Box -> [Int] -> [PixelPoint]
+rawToPixelPoints (Box x y w h) pixels = rawToPixelPointsHelper x w x y pixels
+
+rawToPixelPointsHelper :: Int -> Int -> Int -> Int -> [Int] -> [PixelPoint]
+rawToPixelPointsHelper _ _ _ _ [] = []
+rawToPixelPointsHelper x0 w x y (r:g:b:a:t) = (PixelPoint x y r g b) : if ((x+1) >= w) then rawToPixelPointsHelper x0 w x0 (y+1) t else rawToPixelPointsHelper x0 w (x+1) y t
+rawToPixelPointsHelper _ _ _ _ _ = []
